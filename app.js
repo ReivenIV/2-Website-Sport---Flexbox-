9368a21cfe3ee2ff7a0abdbb7a60410a48adecf5
IV <94102249+ReivenIV@users.noreply.github.com>
2022-02-11 14:12:50 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const btns = document.querySelectorAll('.btn-modale');
+const modaleEquipement = document.querySelector('.bloc-modale');
+const imgIndex = document.querySelector('.bloc-modale img');
+
+
+if(window.matchMedia("(min-width: 850px)").matches){ // Responsive avec JS 
+    
+    btns.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+    
+            imgIndex.src = `ressources/img${e.target.getAttribute('data-index')}-equip.jpg`;
+            modaleEquipement.classList.add('active-modale');
+    
+        })
+    })
+    
+    modaleEquipement.addEventListener('click', () => {
+         modaleEquipement.classList.remove('active-modale');
+    })
+}
+
+
+/* Animation NavBar */
+
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+
+    if(window.scrollY > 30) {
+        nav.classList.add('anim-nav');
+    } else {
+        nav.classList.remove('anim-nav')
+    }
+
+})
